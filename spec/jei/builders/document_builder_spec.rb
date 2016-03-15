e1e7b9ed2ea4d9a2ee9db0e3842c616bbf11fbb1
Michael Macias <zaeleus@gmail.com>
2016-03-15 01:17:07 -0500

    Add tests for top level nodes

--- spec/jei/builders/document_builder_spec.rb
+++ spec/jei/builders/document_builder_spec.rb
@@ -12,6 +12,51 @@ module Jei
           end
         end
 
+        context 'options[:jsonapi] is true' do
+          it 'adds a jsaonapi node to the document' do
+            document = DocumentBuilder.build(nil, jsonapi: true)
+
+            expected = {
+              jsonapi: {
+                version: '1.0'
+              },
+              data: nil
+            }
+
+            expect(document.to_h).to eq(expected)
+          end
+        end
+
+        context 'options[:links] is a list of links' do
+          it 'adds a links node do the document' do
+            links = [Link.new(:self, 'https://example.com/artists')]
+            document = DocumentBuilder.build(nil, links: links)
+
+            expected = {
+              links: {
+                self: 'https://example.com/artists'
+              },
+              data: nil
+            }
+
+            expect(document.to_h).to eq(expected)
+          end
+        end
+
+        context 'options[:meta] is an hash' do
+          it 'adds a meta node to the document' do
+            meta = { total_pages: 10 }
+            document = DocumentBuilder.build(nil, meta: meta)
+
+            expected = {
+              meta: meta,
+              data: nil
+            }
+
+            expect(document.to_h).to eq(expected)
+          end
+        end
+
         context 'options[:serializer] is set to a Serializer class' do
           it 'uses the given serializer class rather than Serializer.factory' do
             artist = Artist.new(id: 1, name: 'FIESTAR')
