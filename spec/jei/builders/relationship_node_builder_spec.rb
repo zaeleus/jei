8597c9123c688505b0e208cb550e489228b10f72
Michael Macias <zaeleus@gmail.com>
2016-03-16 16:03:59 -0500

    Test data object branches when building a RelationshipNode

--- spec/jei/builders/relationship_node_builder_spec.rb
+++ spec/jei/builders/relationship_node_builder_spec.rb
@@ -4,26 +4,67 @@ module Jei
   module Builder
     describe RelationshipNodeBuilder do
       describe '.build' do
-        let(:artist) { Artist.new(id: 1, albums: [Album.new(id: 1)]) }
+        let(:resources) do
+          r = {}
+          r[:artist] = Artist.new(id: 1, albums: [])
+          r[:album] = Album.new(id: 1, artist: r[:artist])
+          r[:artist].albums << r[:album]
+          r
+        end
+
+        let(:artist) { resources[:artist] }
+        let(:album) { resources[:album] }
+
         let(:artist_serializer) { ArtistSerializer.new(artist) }
+        let(:album_serializer) { AlbumSerializer.new(album) }
 
         context 'default' do
-          it 'builds a relationship node' do
-            relationship = HasManyRelationship.new(:albums)
-            node = RelationshipNodeBuilder.build(relationship, artist_serializer)
+          context 'given a belongs-to relationship' do
+            it 'adds a data node with a resource identifier' do
+              relationship = BelongsToRelationship.new(:artist)
+              node = RelationshipNodeBuilder.build(relationship, album_serializer)
 
-            ctx = {}
-            node.visit(ctx)
+              ctx = {}
+              node.visit(ctx)
 
-            expected = {
-              albums: {
-                data: [
-                  { id: '1', type: 'albums' }
-                ]
+              expected = {
+                artist: {
+                  data: { id: '1', type: 'artists' }
+                }
               }
-            }
 
-            expect(ctx).to eq(expected)
+              expect(ctx).to eq(expected)
+            end
+          end
+
+          context 'given a has-many relationship' do
+            it 'adds a collection data node of resource identifiers' do
+              relationship = HasManyRelationship.new(:albums)
+              node = RelationshipNodeBuilder.build(relationship, artist_serializer)
+
+              ctx = {}
+              node.visit(ctx)
+
+              expected = {
+                albums: {
+                  data: [
+                    { id: '1', type: 'albums' }
+                  ]
+                }
+              }
+
+              expect(ctx).to eq(expected)
+            end
+          end
+
+          context 'given an unknown relationship' do
+            it 'raises an error' do
+              relationship = Relationship.new(:albums)
+
+              expect {
+                RelationshipNodeBuilder.build(relationship, artist_serializer)
+              }.to raise_error(ArgumentError)
+            end
           end
         end
 
