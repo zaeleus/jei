e79a832bb9ee1ffb2d651f871055249936efc27c
Michael Macias <zaeleus@gmail.com>
2016-03-24 03:57:22 -0500

    Quick return an empty list if a collection is empty

--- lib/jei/nodes/collection_data_node.rb
+++ lib/jei/nodes/collection_data_node.rb
@@ -2,13 +2,20 @@ module Jei
   module Nodes
     # @see http://jsonapi.org/format/#document-top-level
     class CollectionDataNode < DataNode
+      EMPTY_LIST = [].freeze
+
       # @param [Hash<Symbol, Object>] context
       def visit(context)
-        context[:data] = children.map do |child|
-          data = {}
-          child.visit(data)
-          data
-        end
+        context[:data] =
+          if children.empty?
+            EMPTY_LIST
+          else
+            children.map do |child|
+              data = {}
+              child.visit(data)
+              data
+            end
+          end
       end
     end
   end
